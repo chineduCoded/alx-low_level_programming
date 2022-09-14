04a3b0195b20f0398f0b3dd42f7239e25d26367a
chineduCoded <chinedujohn17@gmail.com>
2022-09-13 18:04:14 -0700

    Editted 104-fibonacci.c file

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,7 +11,8 @@
 
 int main(void)
 {
-	unsigned long int i, j, k, j1, j2, k1, k2;
+	unsigned long i, j, k;
+	unsigned long j1, j2, k1, k2;
 
 	j = 1;
 	k = 2;
@@ -24,15 +25,15 @@ int main(void)
 		j = k - j;
 	}
 
-	j1 = j / 1000000000;
-	j2 = j % 1000000000;
-	k1 = k / 1000000000;
-	k2 = k % 1000000000;
+	j1 = j / 10000000000;
+	j2 = j % 10000000000;
+	k1 = k / 10000000000;
+	k2 = k % 10000000000;
 
 	for (i = 92; i < 99; i++)
 	{
-		printf(", %lu", k1 + (k2 / 1000000000));
-		printf("%lu", k2 % 1000000000);
+		printf(", %lu", k1 + (k2 / 10000000000));
+		printf("%lu", k2 % 10000000000);
 		k1 = k1 + j1;
 		j1 = k1 - j1;
 		k2 = k2 + j2;
