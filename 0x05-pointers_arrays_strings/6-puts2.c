5bc0704ce3dc7f39ef70d69a8ce4eff8975d7068
chineduCoded <chinedujohn17@gmail.com>
2022-09-19 12:52:57 -0700

    Editted 6-puts2.c file

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,14 +8,14 @@
 
 void puts2(char *str)
 {
-	int len = 0, i = 0;
+	int i;
 
-	while (str[len] != '\0')
-		len++;
-
-	len -= 1;
-
-	for (; i < len; i += 2)
-		_putchar(str[i]);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
 	_putchar('\n');
 }
