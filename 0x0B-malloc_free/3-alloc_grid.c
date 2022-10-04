958a58f9af34d9b19ee562e91930c88a98cb6a33
chineduCoded <chinedujohn17@gmail.com>
2022-10-04 06:03:38 -0700

    Refactored 3-alloc_grid.c file

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,41 +2,41 @@
 #include <stdlib.h>
 
 /**
- * alloc_grid - returns a pointer to a 2 dimensional array of integers
- * @width: width of the array.
- * @height: hright of the array.
- *
- * Return: pointer of an array.
+ * alloc_grid - allocates a grid, make space and free space
+ * @width: takes in width of grid
+ * @height: height of grid
+ * Return: grid with freed spaces
  */
+
 int **alloc_grid(int width, int height)
 {
-	int **isarray;
-	int i, n;
+	/*Declaring variables*/
+	int **grid;
+	int i, j;
 
 	if (width <= 0 || height <= 0)
-		return (NULL);
-
-	isarray = malloc(sizeof(int) * height);
-
-	if (isarray == NULL)
 	{
-		free(isarray);
 		return (NULL);
 	}
-
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < height; i++)
 	{
-		isarray[i] = malloc(sizeof(int) * width);
-		if (isarray == NULL)
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
 		{
-			free(isarray);
+			for (i = i - 1; i >= 0; i--)
+			{
+				free(grid[i]);
+			}
+			free(grid);
 			return (NULL);
 		}
-
-		for (n = 0; n < width; n++)
-		{
-			isarray[i][n] = 0;
-		}
 	}
-	return (isarray);
+	for (i = 0; j < width; j++)
+		grid[i][j] = 0;
+	return (grid);
 }
