cd3aa3218f0ac80ec51196b2fe1a85070ead95b6
chineduCoded <chinedujohn17@gmail.com>
2022-10-10 12:28:46 -0700

    Define a new type struct dog with name, age and owner elements

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+# 0x0E. C - Structures, typedef<br/>
+**0. Poppy** - Define a new type struct dog with the following elements: name, type = char *, age, type = float, owner, type = char *<br/><br/>
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef FILE_DOG
+#define FILE_DOG
+
+/**
+ * struct - structure dog
+ * @name: attribute pointer to char name's dog
+ * @age: attribute float age's dog
+ * @owner: attribute pointer to char owner's dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
