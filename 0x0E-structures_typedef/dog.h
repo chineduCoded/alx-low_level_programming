2a918c01a01368c0858269a6cc2bc673d986d13e
chineduCoded <chinedujohn17@gmail.com>
2022-10-10 12:38:19 -0700

    Editted dog.h file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,8 @@
-#ifndef FILE_DOG
-#define FILE_DOG
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * struct - structure dog
+ * struct dog - structure dog
  * @name: attribute pointer to char name's dog
  * @age: attribute float age's dog
  * @owner: attribute pointer to char owner's dog
