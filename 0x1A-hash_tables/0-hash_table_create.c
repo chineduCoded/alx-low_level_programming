6e79ba5f0aba6fb0a611df98dbd0f6fbeec657b5
chineduCoded <chinedujohn17@gmail.com>
2023-01-06 07:00:04 -0800

    fixed error

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,21 +3,21 @@
 /**
  * hash_table_create - creates a hash table
  * @size: the size of the array
- * Return: hash table
+ * Return: pointer to the new table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = malloc(sizeof(hash_table_t));
+	unsigned long int i;
 
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
-	table->array = calloc(size, sizeof(hash_node_t *));
+	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (table->array == NULL)
-	{
-		free(table);
 		return (NULL);
-	}
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
 
 	return (table);
 }
