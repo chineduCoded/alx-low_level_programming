3f7afa3aa4784a9e5c76602f49e7c7183f0217da
chineduCoded <chinedujohn17@gmail.com>
2023-01-06 07:21:04 -0800

    Corrected indentation error in header file

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,17 +7,18 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = malloc(sizeof(hash_table_t));
-	unsigned long int i;
-
+	hash_table_t *table;
+	
+	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
-	table->array = malloc(sizeof(hash_node_t *) * size);
+	table->array = calloc(size, sizeof(hash_node_t *));
 	if (table->array == NULL)
+	{
+		free(table);
 		return (NULL);
-	for (i = 0; i < size; i++)
-		table->array[i] = NULL;
+	}
 
 	return (table);
 }
